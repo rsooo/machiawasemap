2276390867b3a4aae514225e00d9434627628235
rsooo <akira.saito1986@gmail.com>
2013-06-02 16:53:36 +0900

    commit

--- project/Build.scala
+++ project/Build.scala
@@ -10,6 +10,7 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
       // Add your project dependencies here,
       //"postgresql" % "postgresql" % "9.2-1002.jdbc4"
+      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
